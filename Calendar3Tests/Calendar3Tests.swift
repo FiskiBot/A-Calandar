3b3e91a9a588ddfc7946b29363c9227ac24812c7
FiskiBot <patrick.moening@patrickacofdoom.bigfish.lan>
2015-10-28 14:43:42 -0700

    Initial Commit

---
+++ Calendar3Tests/Calendar3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Calendar3Tests.swift
+//  Calendar3Tests
+//
+//  Created by Patrick Moening on 10/28/15.
+//  Copyright © 2015 Fiskisoft. All rights reserved.
+//
+
+import XCTest
+@testable import Calendar3
+
+class Calendar3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
